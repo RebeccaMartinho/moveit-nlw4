bceb947f57840c543797d66cbb6b65115385749b
rebecca martinho <rebeccamartinho@hotmail.com>
2021-02-26 22:17:38 -0300

    aula05

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import '../styles/global.css';
+
+import { ChallengesContext, ChallengesProvider } from '../contexts/ChallengesContext'
+import { useState } from 'react';
+
+function MyApp({ Component, pageProps }) {
+  
+  return (
+      <Component {...pageProps} />
+  )
+}
+
+export default MyApp
